1a9c129e4f4902de540ec4d0cc340afbfa6b45d6
Markus Konkol <m.konkol@52north.org>
2023-04-20 16:22:36 +0200

    print maps with uncertainty

--- code/plotFunctions.R
+++ code/plotFunctions.R
@@ -2,7 +2,7 @@ plotStudyArea <- function(polygon, points){
   plot <- tm_shape(lake_spolydf) + tm_polygons() +
     tm_shape(points_spdf) +
     tm_dots(col="measurement", palette = rev(brewer.pal(7, "RdBu")),
-            title="Sampled precipitation \n(in inches)", size=1) +
+            title="Temperature measurements (C°)", size=1) + ####Correct
     tm_text("measurement", just="left", xmod=.5, size = 0.7) +
     tmap_options(check.and.fix = TRUE) +
     tm_legend(legend.outside=TRUE)
@@ -12,7 +12,7 @@ plotStudyArea <- function(polygon, points){
 plotInterpolationMap <- function(raster, points){
   plot <- tm_shape(raster) + 
     tm_raster(n=10,palette = "RdBu", auto.palette.mapping = FALSE,
-              title="Predicted precipitation \n(in inches)") + 
+              title="Predicted precipitation \n(in inches)") + ####Correct
     tm_shape(points) + tm_dots(size=0.2) +
     tm_legend(legend.outside=TRUE)
   return(plot)
