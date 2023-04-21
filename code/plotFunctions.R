0fa9de287d5c4441abdb8c8511db47733e25c024
Markus Konkol <m.konkol@52north.org>
2023-04-21 13:06:09 +0200

    update Rmd, plot functions

--- code/plotFunctions.R
+++ code/plotFunctions.R
@@ -2,7 +2,7 @@ plotStudyArea <- function(polygon, points){
   plot <- tm_shape(lake_spolydf) + tm_polygons() +
     tm_shape(points_spdf) +
     tm_dots(col="measurement", palette = rev(brewer.pal(7, "RdBu")),
-            title="Temperature measurements (C°)", size=1) + ####Correct
+            title="Temperature measurements (C°)", size=1) +
     tm_text("measurement", just="left", xmod=.5, size = 0.7) +
     tmap_options(check.and.fix = TRUE) +
     tm_legend(legend.outside=TRUE)
@@ -11,8 +11,8 @@ plotStudyArea <- function(polygon, points){
 
 plotInterpolationMap <- function(raster, points){
   plot <- tm_shape(raster) + 
-    tm_raster(n=10,palette = "RdBu", auto.palette.mapping = FALSE,
-              title="Predicted precipitation \n(in inches)") + ####Correct
+    tm_raster(n=10,palette = rev(brewer.pal(7, "RdBu")), auto.palette.mapping = FALSE,
+              title="Temperature measurements (C°)") +
     tm_shape(points) + tm_dots(size=0.2) +
     tm_legend(legend.outside=TRUE)
   return(plot)
